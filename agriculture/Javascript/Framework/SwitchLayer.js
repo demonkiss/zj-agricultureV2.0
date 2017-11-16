92e138fedaa7d6023ebdbc5e4299b9869f635717
captain0520 <captain0520@hotmail.com>
2017-11-16 23:26:13 +0800

    updatehome1116-4

--- agriculture/Javascript/Framework/SwitchLayer.js
+++ agriculture/Javascript/Framework/SwitchLayer.js
@@ -696,8 +696,8 @@ function addDynamicLayer(layersql) {
         imageParameters.transparent = true;
 
         var layerDefs = [];
-       // layerDefs[visiableArray[0]] = layersql;
-        layerDefs[visiableArray[0]] = "";
+        layerDefs[visiableArray[0]] = layersql;
+       // layerDefs[visiableArray[0]] = "";
         imageParameters.layerDefinitions = layerDefs;
         //imageParameters.layerDefinitions = [layersql];
 
